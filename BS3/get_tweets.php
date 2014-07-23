f69681dbdcaacbb8747224feec7ead3e20e977f1
Shota-Shimizu <shimizu@shimizu-no-MacBook-Pro.local>
2014-07-23 22:14:11 +0900

    bootstrap3対応へ
    トップページのみ編集

---
+++ BS3/get_tweets.php
@@ -0,0 +1,60 @@
+<?php
+require_once 'twitteroauth/twitteroauth.php';
+require_once 'fuzzy_time.php';
+
+$consumer_key = 'ANY2AbUdw0itapdkdXEdmVqi4';
+$consumer_secret = 'QZefgPLiUgrDHFtO1ZMGeWfnqYXK6ekLgb2nTq7KZYjZuagLBk';
+$access_token = '261665320-d4FqnxJJG60XfIuXKMHajenkA7xssuXrSCv2ZYaf';
+$access_token_secret = 'otI1d7CmNJfMj5x9w7X0SK1DbNEzcFtsqYw5USZRlF6ob';
+
+$tw_obj = new TwitterOAuth (
+  $consumer_key,
+  $consumer_secret,
+  $access_token,
+  $access_token_secret);
+
+$rest_api = 'https://api.twitter.com/1.1/search/tweets.json';
+$method = 'GET';
+$options = array (
+  'q'=>'乙女の工作部',
+  'result_type'=>'recent',
+  'count'=> 5,
+  'include_entities'=> true);
+
+$tw_obj_request = $tw_obj->OAuthRequest (
+  $rest_api,
+  $method,
+  $options);
+
+$tw_obj_request_json = json_decode($tw_obj_request, true);
+$tweets=array();
+$i=0;
+foreach ($tw_obj_request_json['statuses'] as $item) {
+  $tweet_id = $item['id_str'];
+  $created_at = $item['created_at'];
+  $datetime = date('Y-m-d H:i:s', strtotime($created_at));
+  $datetime = convert_to_fuzzy_time($datetime);
+  $text = $item['text'];
+  foreach($item['entities']['hashtags'] as $tags){
+    $text = str_replace("#".$tags['text'] , '<a href="https://twitter.com/search?q=%23'.$tags['text'].'&src=hash">#'.$tags['text'].'</a>' , $text);
+  }
+  foreach($item['entities']['urls'] as $urls){
+    $text = str_replace($urls['url'] , '<a href="'.$urls['expanded_url'].'">'.$urls['url'].'</a>' , $text);
+  }
+  foreach($item['entities']['user_mentions'] as $mentions){
+    $text = str_replace("@".$mentions['screen_name'] , '<a href="https://twitter.com/'.$mentions['screen_name'].'">@'.$mentions['screen_name'].'</a>' , $text);
+  }
+  foreach($item['entities']['media'] as $media){
+    $text = str_replace($media['url'] , '<br/><a href="'.$media['media_url'].'"><img src="'.$media['media_url'].'"></a>' , $text);
+  }
+  $text = str_replace("\n","<br>",$text);
+  $name = $item['user']['name'];
+  $screen_name = $item['user']['screen_name'];
+  $tweet=array("tweet_id"=>$tweet_id,"datetime"=>$datetime,"text"=>$text,"screen_name"=>$screen_name,"name"=>$name);
+  $tweets[$i]=$tweet;
+  //print_r($item['entities']);
+  $i++;
+}
+$tweets  = json_encode($tweets,JSON_UNESCAPED_SLASHES);
+print_r($tweets);
+?>
