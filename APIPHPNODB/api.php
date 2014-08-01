7ac6c0849cfb9257c20be64c050b45708d84527d
手嶋守 <tejima@gmail.com>
2014-08-01 16:21:56 +0900

    サムネイル表示、画像、スタイル

--- APIPHPNODB/api.php
+++ APIPHPNODB/api.php
@@ -1,20 +1,16 @@
-
 <?php
 require_once 'twitteroauth/twitteroauth.php';
 require_once 'fuzzy_time.php';
-
-function member_info4screenname($screen_name,$tw_obj){
-    $tw_obj_request1 = $tw_obj->OAuthRequest(
-        'https://api.twitter.com/1.1/users/show.json',
-        'GET',
-        array(
-            'screen_name' => $screen_name,
-        )
-    );
+function member_info4screenname($screen_name, $tw_obj)
+{
+    $tw_obj_request1 = $tw_obj->OAuthRequest('https://api.twitter.com/1.1/users/show.json', 'GET', array(
+        'screen_name' => $screen_name,
+    ));
     $tw_obj_request_json1 = json_decode($tw_obj_request1, true);
     return $tw_obj_request_json1;
 }
-function member_list4screenname($owner_screen_name,$tw_obj){
+function member_list4screenname($owner_screen_name, $tw_obj)
+{
     // メンバーをリストから取得
     $options3 = array(
         'slug' => '部員',
@@ -23,10 +19,8 @@ function member_list4screenname($owner_screen_name,$tw_obj){
     $tw_obj_request3 = $tw_obj->OAuthRequest('https://api.twitter.com/1.1/lists/members.json', 'GET', $options3);
     $tw_obj_request_json3 = json_decode($tw_obj_request3, true);
     $i = 0;
-
     $member_screenname_list = array();
-    foreach($tw_obj_request_json3["users"] as $item)
-    {
+    foreach ($tw_obj_request_json3["users"] as $item) {
         $screenname = $item['screen_name'];
         $member_screenname = array(
             "screenname" => $screenname
@@ -36,13 +30,14 @@ function member_list4screenname($owner_screen_name,$tw_obj){
     }
     return $member_screenname_list;
 }
-function tweet_id_list4screen_name($screen_name,$tw_obj){
+function tweet_id_list4screen_name($screen_name, $tw_obj)
+{
     $options1 = array(
         'screen_name' => $screen_name,
         'exclude_replies' => true,
         'include_rts' => false,
         'result_type' => 'recent',
-        'count' => 20,
+        'count' => 10,
         'include_entities' => true
     );
     $tw_obj_request1 = $tw_obj->OAuthRequest('https://api.twitter.com/1.1/statuses/user_timeline.json', 'GET', $options1);
@@ -50,12 +45,12 @@ function tweet_id_list4screen_name($screen_name,$tw_obj){
     $i = 0;
     return $tw_obj_request_json1;
 }
-
-function tweet_id_list4url($url,$tw_obj){
+function tweet_id_list4url($url, $tw_obj)
+{
     $options1 = array(
         'q' => "$url -RT",
         'result_type' => 'recent',
-        'count' => 20,
+        'count' => 10,
         'include_entities' => true
     );
     $tw_obj_request1 = $tw_obj->OAuthRequest('https://api.twitter.com/1.1/search/tweets.json', 'GET', $options1);
@@ -63,71 +58,88 @@ function tweet_id_list4url($url,$tw_obj){
     $i = 0;
     return $tw_obj_request_json1['statuses'];
 }
-
-
-function image_list4screen_name($owner_screen_name,$tw_obj){
+function image_list4screen_name($owner_screen_name, $tw_obj)
+{
     $image_list = array();
     //画像取得2  オーナーのお気に入り写真
-    $tw_obj_request1 = $tw_obj->OAuthRequest(
-        'https://api.twitter.com/1.1/favorites/list.json',
-        'GET',
-        array(
-            'screen_name' => $owner_screen_name,
-            'count' => 100,
-            'include_entities' => true
-        )
-    );
+    $tw_obj_request1 = $tw_obj->OAuthRequest('https://api.twitter.com/1.1/favorites/list.json', 'GET', array(
+        'screen_name' => $owner_screen_name,
+        'count' => 100,
+        'include_entities' => true
+    ));
     $tw_obj_request_json1 = json_decode($tw_obj_request1, true);
-    foreach($tw_obj_request_json1 as $item)
-    {
-        foreach($item['entities']['media'] as $media)
-        {
+    foreach ($tw_obj_request_json1 as $item) {
+        foreach ($item['entities']['media'] as $media) {
             $image_list[] = $media['media_url'];
         }
     }
     shuffle(array_unique($image_list));
     return array_slice($image_list, 0, 20);
 }
-function image_list4url($url,$tw_obj){
+function image_list4url($url, $tw_obj)
+{
     //画像取得１ 部活動urlが含まれる画像
-    $tw_obj_request1 = $tw_obj->OAuthRequest(
-        'https://api.twitter.com/1.1/search/tweets.json',
-        'GET',
-        array(
-            'q' => "$url -RT",
-            'result_type' => 'recent',
-            'count' => 100,
-            'lang' => 'ja',
-            'include_entities' => true,
-        )
-    );
+    $tw_obj_request1 = $tw_obj->OAuthRequest('https://api.twitter.com/1.1/search/tweets.json', 'GET', array(
+        'q' => "$url -RT",
+        'result_type' => 'recent',
+        'count' => 100,
+        'lang' => 'ja',
+        'include_entities' => true,
+    ));
     $tw_obj_request_json1 = json_decode($tw_obj_request1, true);
     $image_list = array();
-    foreach($tw_obj_request_json1['statuses'] as $item)
-    {
-        foreach($item['entities']['media'] as $media)
-        {
+    foreach ($tw_obj_request_json1['statuses'] as $item) {
+        foreach ($item['entities']['media'] as $media) {
             $image_list[] = $media['media_url'];
         }
     }
     shuffle(array_unique($image_list));
     return array_slice($image_list, 0, 20);
 }
-
-$club["recorder"] = array( 
-    "club_name"=>"recorder",
-    "club_name_long"=>"リコーダー部", 
-    "keyword"=>"リコーダー", 
-    "description"=>"リコーダーが好きな人が集まって、持ち寄った曲を合奏します。初心者から上級者まで楽しく演奏するのが目的です。演奏した曲はyoutubeで公開していきます！",
-    "regular_activity"=>"月１〜２回",
-    "use_budget"=>"楽譜代",
-    "leader_screenname"=>"sho_qu", 
-    "place"=>"新宿区、早稲田大学戸山キャンパス学生会館", 
-    "mail_address"=>"kpdyd122@gmail.com",
+$club["buto"] = array(
+    "club_name" => "buto",
+    "club_name_long" => "舞踏倶楽部",
+    "keyword" => "舞踏",
+    "description" => "舞踏倶楽部は、社交ダンスのリーダー、パートナーなど、ダンスのお相手やダンス友達と出会えて踊れる、日本で初めての社交ダンス専門の会員制ダンサーコミュニティサイトです。",
+    "regular_activity" => "月に都合のつく回数",
+    "use_budget" => "場所代",
+    "leader_screenname" => "butoclub",
+    "place" => "東京",
+    "mail_address" => "info@buto.jp",
+    "bot_mode" => false,
+    "member_total" => 1,
+    "member_last" => 30,
+    "member_new" => 0,
+);
+$club["BoulderingInTakadanobaba"] = array(
+    "club_name" => "BoulderingInTakadanobaba",
+    "club_name_long" => "高田馬場駅ボルダリング部",
+    "keyword" => "ボルダリング",
+    "description" => "新しく何か始めたい人、ボルダリングしましょう！高田馬場駅周辺でボルダリングをします！学生の方はもちろん、社会人の方もぜひ！",
+    "regular_activity" => "月4〜6回、ボルダリングジムでボルダリングを行う",
+    "use_budget" => "ボルダリングジムの利用料金 など",
+    "leader_screenname" => "Akhr5884",
+    "place" => "東京都新宿区",
+    "mail_address" => "a.kobayashi@tejimaya.com",
     "bot_mode" => false,
     "member_total" => 1,
     "member_last" => 4,
-    "member_new" => 0,    
+    "member_new" => 0,
+);
+$club["recorder"] = array(
+    "club_name" => "recorder",
+    "club_name_long" => "リコーダー部",
+    "keyword" => "リコーダー",
+    "description" => "リコーダーが好きな人が集まって、持ち寄った曲を合奏します。初心者から上級者まで楽しく演奏するのが目的です。演奏した曲はyoutubeで公開していきます！",
+    "regular_activity" => "月１〜２回",
+    "use_budget" => "楽譜代",
+    "leader_screenname" => "sho_qu",
+    "place" => "新宿区、早稲田大学戸山キャンパス学生会館",
+    "mail_address" => "kpdyd122@gmail.com",
+    "bot_mode" => false,
+    "member_total" => 1,
+    "member_last" => 4,
+    "member_new" => 0,
 );
 $club["otome"] = array(
     "club_name" => "otome",
@@ -142,7 +154,7 @@ $club["otome"] = array(
     "bot_mode" => true,
     "member_total" => 5,
     "member_last" => 5,
-    "member_new" => 0,        
+    "member_new" => 0,
 );
 $club["soccer"] = array(
     "club_name" => "soccer",
@@ -157,8 +169,7 @@ $club["soccer"] = array(
     "bot_mode" => false,
     "member_total" => 1,
     "member_last" => 0,
-    "member_new" => 0,    
-
+    "member_new" => 0,
 );
 $club["kuzu"] = array(
     "club_name" => "kuzu",
@@ -173,81 +184,96 @@ $club["kuzu"] = array(
     "bot_mode" => false,
     "member_total" => 1,
     "member_last" => 9,
-    "member_new" => 0,    
+    "member_new" => 0,
 );
 $club["onnobesendenbu"] = array(
-    "club_name"=>"onnobesendenbu",
-    "club_name_long"=>"オンノベ宣伝部",
-    "keyword"=>"自作小説の宣伝",
-    "description"=>"自作オンラインノベルをいろんな人に読んでもらいたい。そのためオフでも宣伝活動行います。",
-    "regular_activity"=>"文学フリマ",
-    "use_budget"=>"イベント参加費、本の印刷費など",
-    "leader_screenname"=>"iduha_sousaku",
-    "place"=>"オンライン",
-    "mail_address"=>"orpimentffd800@gmail.com",
+    "club_name" => "onnobesendenbu",
+    "club_name_long" => "オンノベ宣伝部",
+    "keyword" => "自作小説の宣伝",
+    "description" => "自作オンラインノベルをいろんな人に読んでもらいたい。そのためオフでも宣伝活動行います。",
+    "regular_activity" => "文学フリマ",
+    "use_budget" => "イベント参加費、本の印刷費など",
+    "leader_screenname" => "iduha_sousaku",
+    "place" => "オンライン",
+    "mail_address" => "orpimentffd800@gmail.com",
     "bot_mode" => false,
     "member_total" => 1,
     "member_last" => 5,
-    "member_new" => 0,    
-
+    "member_new" => 0,
 );
 $club["cfes"] = array(
-    "club_name"=>"challengedfes",
-    "club_name_long"=>"チャレンジドフェスティバル応援部",
-    "keyword"=>"フェスティバル",
-    "description"=>"チャレンジド・フェスティバル実行委員会では、継続的なイベント開催を目標としています。そのためには、イベントを応援してくれる応援部が必要だと感じています。そこで、チャレンジド・フェスティバル応援部を立ち上げました。",
-    "regular_activity"=>"メンバーお茶会、イベントボランティア、セミナー",
-    "use_budget"=>"イベントの運営費",
-    "leader_screenname"=>"challengedfes",
-    "place"=>"東京都内",
-    "mail_address"=>"info@challenged-festival.jp",
+    "club_name" => "cfes",
+    "club_name_long" => "チャレンジド・フェスティバル応援部",
+    "keyword" => "応援",
+    "description" => "知的障がいを持つ子ども・青年70名とその母親たち30名が出演するミュージカルです。継続的なイベント開催のため、イベントを応援してくれる応援部員を求めています。",
+    "regular_activity" => "年1回のダンスイベント・その他セミナー開催",
+    "use_budget" => "イベント資金",
+    "leader_screenname" => "challengedfes",
+    "place" => "東京都千代田区",
+    "mail_address" => "info@challenged-festival.jp",
     "bot_mode" => true,
-    "member_total" => 2,
-    "member_last" => 28,
-    "member_new" => 0,    
+    "member_total" => 7,
+    "member_last" => 23,
+    "member_new" => 5,
 );
-
-
-
-
-
-$domain = $_GET["domain"] ?: 'otome';
+$club["trpg"] = array(
+    "club_name" => "trpg",
+    "club_name_long" => "TRPGSNS",
+    "keyword" => "TRPG",
+    "description" => "ＴＲＰＧの情報とプレイ機会をシェアするためのＳＮＳです。参加すると、気軽にプライベート・セッションの企画や、コンベンションや例会、オンラインセッションなどの告知が可能となります。また、同好の士が集まるので、プレイに誘われる機会が増えるかも知れません。",
+    "regular_activity" => "自由",
+    "use_budget" => "サイト運営費",
+    "leader_screenname" => "inouekari",
+    "place" => "オンライン",
+    "mail_address" => "",
+    "bot_mode" => false,
+    "member_total" => 758,
+    "member_last" => 30,
+    "member_new" => 0,
+);
+$club["kaosknight"] = array(
+    "club_name" => "kaosknight",
+    "club_name_long" => "混沌の騎士団",
+    "keyword" => "西洋魔術",
+    "description" => "最先端の魔術で時代をリードする混沌の騎士団",
+    "regular_activity" => "一日30分",
+    "use_budget" => "指輪代",
+    "leader_screenname" => "kontonnokisidan",
+    "place" => "日本",
+    "mail_address" => "postmaster@konton-no-kisidan.jp",
+    "bot_mode" => true,
+    "member_total" => 1,
+    "member_last" => 10,
+    "member_new" => 0,
+);
+$domain = $_GET["domain"] ? : 'otome';
 $club_info = $club[$domain];
-
 $url = $domain . "." . $_SERVER["SERVICE_DOMAIN"];
 $tw_obj = new TwitterOAuth($_SERVER['CONSUMER_KEY'], $_SERVER['CONSUMER_SECRET'], $_SERVER['ACCESS_TOKEN'], $_SERVER['ACCESS_TOKEN_SECRET']);
-
-apc_clear_cache("user");
-
-
-if($club_info["bot_mode"]){
-    $member_screenname_list = apc_fetch("member_screenname_list:".$url,$is_success_member_screenname_list) ?: member_list4screenname($club_info["leader_screenname"],$tw_obj);
-
-    $image_list = apc_fetch("image_list:".$url,$is_success_image_list) ?: image_list4screen_name($club_info["leader_screenname"],$tw_obj);
-    $tweet_list = apc_fetch("tweet_list:".$url,$is_success_tweet_list) ?: tweet_id_list4screen_name($club_info["leader_screenname"],$tw_obj);
-    $twitter_member =  apc_fetch("member_info:".$url,$is_success_twitter_member) ?: member_info4screenname($club_info["leader_screenname"],$tw_obj);
+//apc_clear_cache("user");
+if ($club_info["bot_mode"]) {
+    $member_screenname_list = apc_fetch("member_screenname_list:" . $url, $is_success_member_screenname_list) ? : member_list4screenname($club_info["leader_screenname"], $tw_obj);
+    $image_list = apc_fetch("image_list:" . $url, $is_success_image_list) ? : image_list4screen_name($club_info["leader_screenname"], $tw_obj);
+    $tweet_list = apc_fetch("tweet_list:" . $url, $is_success_tweet_list) ? : tweet_id_list4screen_name($club_info["leader_screenname"], $tw_obj);
+    $twitter_member = apc_fetch("member_info:" . $url, $is_success_twitter_member) ? : member_info4screenname($club_info["leader_screenname"], $tw_obj);
     $club_info["background"] = $twitter_member["profile_banner_url"];
-
-}else{
-    $member_screenname_list = apc_fetch("member_screenname_list:".$url,$is_success_member_screenname_list) ?: member_list4screenname($club_info["leader_screenname"],$tw_obj);
-
-    $image_list = apc_fetch("image_list:".$url,$is_success_image_list) ?: image_list4url($url,$tw_obj);
-    $tweet_list = apc_fetch("tweet_list:".$url,$is_success_tweet_list) ?: tweet_id_list4url($url,$tw_obj);
+} else {
+    $member_screenname_list = apc_fetch("member_screenname_list:" . $url, $is_success_member_screenname_list) ? : member_list4screenname($club_info["leader_screenname"], $tw_obj);
+    $image_list = apc_fetch("image_list:" . $url, $is_success_image_list) ? : image_list4url($url, $tw_obj);
+    $tweet_list = apc_fetch("tweet_list:" . $url, $is_success_tweet_list) ? : tweet_id_list4url($url, $tw_obj);
 }
-
-if(!$is_success_twitter_member){
-    apc_store("member_info:".$url,$twitter_member,120);
+if (!$is_success_twitter_member) {
+    apc_store("member_info:" . $url, $twitter_member, 600);
 }
-if(!$is_success_member_screenname_list){
-    apc_store("member_screenname_list:".$url,$member_screenname_list,120);
+if (!$is_success_member_screenname_list) {
+    apc_store("member_screenname_list:" . $url, $member_screenname_list, 600);
 }
-if(!$is_success_image_list){
-    apc_store("image_list:".$url,$image_list,120);
+if (!$is_success_image_list) {
+    apc_store("image_list:" . $url, $image_list, 600);
 }
-if(!$is_success_tweet_list){
-    apc_store("tweet_list:".$url,$tweet_list,120);
+if (!$is_success_tweet_list) {
+    apc_store("tweet_list:" . $url, $tweet_list, 600);
 }
-
 $data = array(
     "club_info" => $club_info,
     "image_list" => $image_list,
